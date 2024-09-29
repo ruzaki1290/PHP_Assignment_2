5d23b231d95ff1b3ba747581230f18cbc0493aba
Rus <rus.zakirov@triosstudent.com>
2024-09-29 11:13:04 -0400

    Added tech support directory with index.html in it and modified code in some other files

--- tech_support/product_manager/index.php
+++ tech_support/product_manager/index.php
@@ -1,16 +1,31 @@
-<?php
-require('../model/database.php');
-// require('../model/product_db.php');
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Manage Products</title>
+        <link rel="stylesheet" type="text/css"
+            href="/Rus_PHP_2/tech_support/main.css">
+    </head>
+    <body>
+        <?php include 'view/header.php'; ?>
+        <main>
+            <?php
+                require('../model/database.php');
+                // require('../model/product_db.php');
 
-$action = filter_input(INPUT_POST, 'action');
-if ($action === NULL) {
-    $action = filter_input(INPUT_GET, 'action');
-    if ($action === NULL) {
-        $action = 'under_construction';
-    }
-}
+                $action = filter_input(INPUT_POST, 'action');
+                if ($action === NULL) {
+                    $action = filter_input(INPUT_GET, 'action');
+                    if ($action === NULL) {
+                        $action = 'under_construction';
+                    }
+                }
 
-if ($action == 'under_construction') {
-    include('../under_construction.php');
-}
-?>
+                if ($action == 'under_construction') {
+                    include('../under_construction.php');
+                }
+            ?>
+        </main>   
+    </body>
+</html>
---
+++ tech_support/technician_manager/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Manage Technicians</title>
+        <link rel="stylesheet" type="text/css"
+            href="/Rus_PHP_2/tech_support/main.css">
+    </head>
+    <body>
+        <?php include 'view/header.php'; ?>
+        <main>
+            <?php
+                require('../model/database.php');
+                // require('../model/product_db.php');
+
+                $action = filter_input(INPUT_POST, 'action');
+                if ($action === NULL) {
+                    $action = filter_input(INPUT_GET, 'action');
+                    if ($action === NULL) {
+                        $action = 'under_construction';
+                    }
+                }
+
+                if ($action == 'under_construction') {
+                    include('../under_construction.php');
+                }
+            ?>
+        </main>   
+    </body>
+</html>
